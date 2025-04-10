cc793a9dfbe49012bfc62b02a3f8b65a2ae5f65a
Deepanshu Rana <deepanshu.rana2021@vitstudent.ac.in>
2025-04-11 04:09:54 +0530

    Developed markets page- added dummy stock data

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Treasures from "./components/Dashboard/treasure";
 import Savings from "./components/Dashboard/saving";
 import ReduxProvider from "./components/ReduxProvider";
 import ErrorBoundary from "./components/ErrorBoundary";
+import Market from "./components/Markets/Market";
 
 const AppContent = () => {
   return (
@@ -17,6 +18,7 @@ const AppContent = () => {
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/home" element={<Home />} />
         <Route path="/landing" element={<Landing />} />
+        <Route path="/markets" element={<Market />} />
         <Route path="/dashboard" element={
           <ErrorBoundary>
             <Dashboard />
